d273561506be27e4222f32bff810562980061afc
Arshiya Norpour <arshiyanorpour@gmail.com>
2022-01-15 13:20:48 +0330

    Added some function template and some function argument changed!

--- ArrayHelper.py
+++ ArrayHelper.py
@@ -1,2 +1,8 @@
-def RandomArray():
+def RandomArray(min, max, number):
+    pass
+def WriteArray(location):
+    pass
+def AddArray(array1, array2):
+    pass
+def DeleteArray(array1, array2):
     pass
