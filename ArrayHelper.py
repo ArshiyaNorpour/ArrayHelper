7e89102304b3f29fa515ef6460e16cc042a5b4ad
Arshiya Norpour <arshiyanorpour@gmail.com>
2022-01-16 13:32:10 +0330

    Added 7 function template & deleted a function!

--- ArrayHelper.py
+++ ArrayHelper.py
@@ -1,9 +1,21 @@
 def RandomArray(min, max, number):
     pass
-def WriteArray(array, location):
-    pass
 def AddArray(array1, array2):
     for i in range(len(array2)):
         array1.append(array2[i])
 def DeleteArray(array1, array2):
+    pass
+def NumberArray(array):
+    pass
+def StringArray(array):
+    pass
+def LastNumberArray(array):
+    pass
+def LastStringArray(array):
+    pass
+def ValueArrays(array1, array2):
+    pass
+def ExchangeSortArray(array):
+    pass
+def LastArray(array):
     pass
